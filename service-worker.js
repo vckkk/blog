f0e515d1b6e003e1e8a1b6ce3b7682f6ced0c6d0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-04 15:51:23 +0000

    Deploy vckkk/vckkk.github.io to github.com/vckkk/vckkk.github.io.git:gh-pages

---
+++ service-worker.js
@@ -0,0 +1,136 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "a7c5d4a549b32aad345bd5fc8fd820f5"
+  },
+  {
+    "url": "assets/css/0.styles.fef5045a.css",
+    "revision": "34c451d1b76265772efe811d94d7fbf0"
+  },
+  {
+    "url": "assets/img/search.83621669.svg",
+    "revision": "83621669651b9a3d4bf64d1a670ad856"
+  },
+  {
+    "url": "assets/js/10.2be47945.js",
+    "revision": "597eba097b1c82694403a4d843e0a5db"
+  },
+  {
+    "url": "assets/js/11.c10c468c.js",
+    "revision": "0f07c82723ad6950313b95bdeae3cb14"
+  },
+  {
+    "url": "assets/js/12.c91a394a.js",
+    "revision": "84f39645c17ecd371e7eaa3902ee9257"
+  },
+  {
+    "url": "assets/js/2.51671d88.js",
+    "revision": "07b76a1c3d5e1fdd1464baa3f4ba8ec4"
+  },
+  {
+    "url": "assets/js/3.136b693b.js",
+    "revision": "3322330aefc63c827112735f7f8170d1"
+  },
+  {
+    "url": "assets/js/4.5262c1f8.js",
+    "revision": "f9a966ca1e5cf504babf201c111e25e6"
+  },
+  {
+    "url": "assets/js/5.e1836220.js",
+    "revision": "168891ae8c1ed2e54cb3fbcc246413e3"
+  },
+  {
+    "url": "assets/js/6.cd28b8ee.js",
+    "revision": "87992b9ad6135ef0becf2e5a4628a209"
+  },
+  {
+    "url": "assets/js/7.dd579077.js",
+    "revision": "5cc2f49d8e120d17693089910acae3bc"
+  },
+  {
+    "url": "assets/js/8.d06dea88.js",
+    "revision": "2c276c815f2d6c7f0a40354045c77056"
+  },
+  {
+    "url": "assets/js/9.96e1c7af.js",
+    "revision": "2bd78d34555f94c487108d18ecc96adf"
+  },
+  {
+    "url": "assets/js/app.ea509887.js",
+    "revision": "0e8b1f8b5b26bd3cde9200b0dbcafd32"
+  },
+  {
+    "url": "favicon.png",
+    "revision": "4b101024f5aac0839a028e6819df3fa0"
+  },
+  {
+    "url": "index.html",
+    "revision": "08031e04ae22a3fc17f3803e9f8f2a36"
+  },
+  {
+    "url": "Pages/ali.html",
+    "revision": "c6f3f30d019e93224b231b35e6d80729"
+  },
+  {
+    "url": "Pages/demo.html",
+    "revision": "7f369cba432e558dcd5df0b8326491db"
+  },
+  {
+    "url": "Pages/ES6.html",
+    "revision": "24b1fc53aa44d185b95180aac87df6da"
+  },
+  {
+    "url": "Pages/index.html",
+    "revision": "8eb7479a2886c6bbb216fe0d3babc677"
+  },
+  {
+    "url": "Pages/JavaScript.html",
+    "revision": "65ef4571c8bc4c09d24f7dcc7a764db5"
+  },
+  {
+    "url": "Pages/Reactjs.html",
+    "revision": "924b3200917b02ce21284bf4cf5d1fab"
+  },
+  {
+    "url": "Pages/网络基础.html",
+    "revision": "b64084166e5d20a3cd0a93488a0b5f1e"
+  },
+  {
+    "url": "Pages/阿里react全家桶.html",
+    "revision": "570d6ac5924226dadf686d35594f94cf"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
