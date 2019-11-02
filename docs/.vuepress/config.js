fcb09f72821a816bba55787f9ce550d436c605b8
vckkk <info@webhbz.com>
2019-11-02 11:27:06 +0800

    Update cover

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   theme: 'ououe',
-  title: 'Vckkk',
+  title: 'Vckkk‘blog',
   description: 'Do what you want',
+  vergreen: true,
   head: [
     ['link', {
       rel: 'icon',
@@ -14,13 +15,14 @@ module.exports = {
   themeConfig: {
     defaultTheme: 'dark',
     activeHeaderLinks: false,
+    cover: '/cover.jpg',
     // displayAllHeaders: true,
     nav: [{
-        text: 'Home',
+        text: '',
         link: '/'
       },
       {
-        text: 'Pages',
+        text: '',
         link: '/Pages/'
       },
       {
@@ -32,37 +34,10 @@ module.exports = {
         link: '/Daily/'
       }
     ],
-//     sidebar: {
-//       '/Pages/': [
-//         '',
-//         'ES6',
-//         'JavaScript',
-//         'Reactjs',
-//         'demo',
-//         'ali',
-//         'traversal',
-//         'eventloop',
-//         'this',
-//         'prototype'
-//       ],
-//       '/Flutter/': [
-//         '',
-//         'Dart',
-//         'communication',
-//         'routenav',
-//         'pointer',
-//         'dio'
-
-//       ],
-//       '/Daily/': [
-//         '',
-//         '0507',
-//         '0521'
-//       ]
-//     },
-//     sidebarDepth: 0,
     search: true,
+    useVssue: true,
     searchMaxSuggestions: 10,
+    backgroundImage: true,
     // lastUpdated: '最后更新于：',
     postTime: {
       createTime: '创建时间',
@@ -72,10 +47,19 @@ module.exports = {
     docsBranch: 'gh-pages'
 
   },
- postcss: {
-   plugins: [
-     require('css-prefers-color-scheme/postcss'),
+  postcss: {
+    plugins: [
+      require('css-prefers-color-scheme/postcss'),
       require('autoprefixer')
     ]
-  }
+  },
+  plugins: [
+    ['@vssue/vuepress-plugin-vssue', {
+      platform: 'github',
+      owner: 'vckkk',
+      repo: 'blog',
+      clientId: 'MDQ6VXNlcjM3MzI0MjIx',
+      clientSecret: 'c8f21f3c47c6e78f5e51e72e55a44f019a91ef8b'
+    }],
+  ]
 }
